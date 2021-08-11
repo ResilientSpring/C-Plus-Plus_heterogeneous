382b181a3c4b1ca34700fd3a078fe755d664a5bd
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-08-11 16:10:07 +0800

    Committed 2021/08/11

--- Passing_strings_to_functions/Prominent.cpp
+++ Passing_strings_to_functions/Prominent.cpp
@@ -1,13 +1,12 @@
 #include <string>
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-void strInvertCase(char* str);
-
 int main() {
-
+	string str = "This Is A Test.";
+	transform(str.begin(), str.end(), str.begin(), toupper);
+	cout << str;
 }
 
-void strInvertCase(char* str) {
-
-}
+// Reference: https://stackoverflow.com/a/60578519/14900011
