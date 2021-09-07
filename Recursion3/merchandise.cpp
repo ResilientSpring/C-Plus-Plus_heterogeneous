2f1c60f32ce058757012efc02cf5c7b91dc7c5dc
Ceiling_roof <ted10014@hotmail.com>
2021-09-07 12:14:53 +0800

    Committed 2021/09/07

---
+++ Recursion3/merchandise.cpp
@@ -0,0 +1,26 @@
+// Print a string backwards using recursion.
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void reverse(string s);
+
+int main() {
+	string str = "this is a test";
+
+	reverse(str);
+
+	return 0;
+}
+
+// Print string backwards.
+void reverse(string s) {
+	if (s[0] != 0)
+	{
+		for (int i = 0; i < s.length; i++)
+		{
+
+		}
+	}
+}
