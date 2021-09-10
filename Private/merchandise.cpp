a334042194c2d7a9c4a876ed44592c2a4383ec26
Ceiling_roof <ted10014@hotmail.com>
2021-09-10 12:57:59 +0800

    Committed 2021/09/10

---
+++ Private/merchandise.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Vehicle {
+	int passengers;
+	int fuelcap;
+	int mpg;
+	
+};
+
+int main() {
+	Vehicle minivan;
+	int range;
+
+	minivan.fuelcap = 16;
+}
