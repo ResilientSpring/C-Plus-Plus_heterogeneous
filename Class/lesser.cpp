2aa20d196422d5e41a4166bfe741d34aea13172a
Ceiling_roof <ted10014@hotmail.com>
2021-09-10 12:11:04 +0800

    Committed 2021/09/10

---
+++ Class/lesser.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Vehicle {
+public:
+	int passenger; // number of passengers.
+	int fuelcap; // fuel capacity in gallons .
+	int mpg; // fuel consumption in miles per gallon.
+};
