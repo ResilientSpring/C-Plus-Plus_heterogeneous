247c87fa0e5ecb993a359c459b07b2904c3c9ddd
Ceiling_roof <ted10014@hotmail.com>
2021-09-10 14:35:11 +0800

    Committed 2021/09/10

---
+++ Objects/suit_up.cpp
@@ -0,0 +1,19 @@
+// This program creates two Vehicle objects. 
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+// Declare the Vehicle class. 
+class Vehicle {
+public:
+	int passengers;  // number of passengers 
+	int fuelcap;  // fuel capacity in gallons. 
+	int mpg; // fuel consumption in miles per gallons.
+};
+
+int main() {
+	Vehicle minivan;  // Create a Vehicle object;
+	Vehicle sportscar; // create another object.
+
+}
