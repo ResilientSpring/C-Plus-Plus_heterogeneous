21af998f1c103fcd30eeb27590b6b0ef7bd0fb45
Ceiling_roof <ted10014@hotmail.com>
2021-09-10 12:35:57 +0800

    Committed 2021/09/10

--- Class/lesser.cpp
+++ Class/lesser.cpp
@@ -8,3 +8,19 @@ public:
 	int fuelcap; // fuel capacity in gallons .
 	int mpg; // fuel consumption in miles per gallon.
 };
+
+int main() {
+	Vehicle minivan;  // Create a Vehicle object called minivan.
+	int range;
+
+	minivan.fuelcap = 16;
+	minivan.passenger = 7;
+	minivan.mpg = 21;
+
+	// Compute the range assuming a full tank of gas.
+	range = minivan.fuelcap * minivan.mpg;
+
+	cout << "Minivan can carry " << minivan.passenger << " with a range of " << range << "\n";
+
+	return 0;
+}
