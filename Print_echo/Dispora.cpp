e7bf30732b44abcede38f9d27694553d27a124b3
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-10-05 10:06:27 +0800

    Committed 2021/10/05

---
+++ Print_echo/Dispora.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int x = 10;
+	int y = 5; 
+	int z = 3; 
+
+	int sum = x + y;
+
+	cout << x << "+" << y << "=" << sum;
+
+	return 0;
+}
