7c363ae483d3501b79e295f90b6f7bcebe5b6f74
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-10-05 16:04:38 +0800

    Committed 2021/10/05

--- Arrays_of_pointers/diaspora.cpp
+++ Arrays_of_pointers/diaspora.cpp
@@ -4,4 +4,22 @@ using namespace std;
 int main() {
 	// In a declaration statement, preceding a variable name with a * causes that variable to be a pointer.
 	int* pi[10];
+
+	// ----- End of declaration statements.
+
+	int element = 520;
+
+	pi[0] = &element;
+
+	//  * is an operator representing the value at the memory address of its operand. 
+	cout << *pi[0];
+
+
+	// --------------------
+
+	int coincide[5] = { 123, 456, 789, 101112, 131415 };
+
+	cout << "\n" << coincide[0];
+
+	return 0;
 }
