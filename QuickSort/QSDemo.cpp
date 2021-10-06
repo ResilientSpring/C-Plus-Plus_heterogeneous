d1595a1573982f9dcf51edcf86b63012a04938b8
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-10-06 15:24:54 +0800

    Committed 2021/10/06

--- QuickSort/QSDemo.cpp
+++ QuickSort/QSDemo.cpp
@@ -4,3 +4,17 @@ using namespace std;
 int main() {
 
 }
+
+// Provide a convenient interface for the user and sets up a call to the actual sorting function called qs().
+void quick_sort(char *items, int len) {
+
+	// items points to the array to be sorted.[1]
+	qs(items, 0, len - 1);
+}
+
+/*
+* References:
+* 1. array name indicates the address of first element and arrays are always passed as pointers.
+* https://www.geeksforgeeks.org/arrays-in-c-cpp/
+
+*/
