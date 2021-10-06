5f00991641082f07911ba0c8ff2d1db24491647a
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-10-06 17:18:49 +0800

    Committed 2021/10/06

--- QuickSort/QSDemo.cpp
+++ QuickSort/QSDemo.cpp
@@ -1,15 +1,53 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
+void quick_sort(char* array_name, int len);
+void qs(char* array_name, int left, int right);
+
+// The version we develop sorts a character array, but the logic can be adapted to sort any type of object.
 int main() {
+
+
 
 }
 
 // Provide a convenient interface for the user and sets up a call to the actual sorting function called qs().
-void quick_sort(char *items, int len) {
+void quick_sort(char *array_name, int len) {
 
 	// items points to the array to be sorted.[1]
-	qs(items, 0, len - 1);
+	qs(array_name, 0, len - 1);
+}
+
+// A recursive version of Quicksort for sorting characters.
+void qs(char* array_name, int left, int right) {
+	int i = left;
+	int j = right;
+	char x = array_name[(left + right) / 2];
+	char y;
+
+	do {
+		while ((array_name[i] < x) && (i < right))
+			i++;
+		while ((x < array_name[j]) && (j > left))
+			j--;
+
+		if (i <= j) {
+			y = array_name[i];
+			array_name[i] = array_name[j];
+			array_name[j] = y;
+
+			i++;
+			j--;
+		}
+
+	} while (i <= j);
+
+	if (left < j)
+		qs(array_name, left, j);
+
+	if (i < right)
+		qs(array_name, i, right);
 }
 
 /*
