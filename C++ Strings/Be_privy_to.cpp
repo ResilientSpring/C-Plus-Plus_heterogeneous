1d532b831768e6a963476d851d651f3d981b23d7
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-10-07 13:13:42 +0800

    Committed 2021/10/07

---
+++ C++ Strings/Be_privy_to.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int main() {
+	string belated = "prevaricate";
+
+	string at_large[] = { "Society at large will benefit from this.",
+		"This group is not representative of the population at large." };
+
+	cout << at_large[0];
+
+	return 0;
+}
