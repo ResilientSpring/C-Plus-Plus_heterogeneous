fe751d4bc5ad15d42296d3fa68f971be6cd76383
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2021-11-07 16:55:25 +0800

    Committed 2021/11/07

--- Module5 Mastery Check 2/Bulk.cpp
+++ Module5 Mastery Check 2/Bulk.cpp
@@ -21,12 +21,19 @@ int main() {
 	double the_hypotenuse = hypotenuse(side1, side2);
 
 	cout << "The hypotenuse of the right triangle given the lengths of the two opposing sides " 
-		+ to_string(side1) + " and " + to_string(side2) + " is " + to_string(the_hypotenuse);
+		+ to_string(side1) + " and " + to_string(side2) + " is " + to_string(the_hypotenuse); // [1]
 
 	return 0;
 
 }
 
 double hypotenuse(double opposing_side1, double opposing_side2) {
-	return sqrt((pow(opposing_side1, 2) + pow(opposing_side2, 2)));
+	return sqrt((pow(opposing_side1, 2) + pow(opposing_side2, 2)));  // [2]
 }
+
+// Reference: 
+// 1. How to concatenate a string and an int
+// https://stackoverflow.com/a/900035
+
+// 2. C++ pow()
+// https://www.programiz.com/cpp-programming/library-function/cmath/pow
