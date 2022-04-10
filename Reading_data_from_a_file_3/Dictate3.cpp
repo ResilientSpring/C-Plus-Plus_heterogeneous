a4b407236b76c62070575ee5f5c9fbb795ba5e2c
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-04-10 19:14:08 +0800

    Committed 2022/04/10

---
+++ Reading_data_from_a_file_3/Dictate3.cpp
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdlib.h>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+	FILE* inputFile; // Read as "file pointer in"
+
+	inputFile = fopen("trace.txt", "r");  // Open the file for "reading".
+}
