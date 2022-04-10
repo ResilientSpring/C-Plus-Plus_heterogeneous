c6e10dacdaeb80626028748e1709bb9765e9c415
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-04-10 16:51:27 +0800

    Committed 2022/04/10

---
+++ Pause/Be_privy_to2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int main() {
+	string belated = "prevaricate";
+
+	string at_large[] = { "Society at large will benefit from this.",
+		"This group is not representative of the population at large." };
+
+	cout << at_large[0];
+
+	system("pause");
+
+	return 0;
+}
