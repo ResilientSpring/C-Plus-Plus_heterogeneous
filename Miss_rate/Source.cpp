5cf2b082d2ed8f87aa9b7dd71ad47253fc7fd3c2
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-04-11 23:19:46 +0800

    Committed 2022/04/11

---
+++ Miss_rate/Source.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	int a, b, c, d[5002];
+	int block_size;
+	int location[5002];
+	int cache_size_kilobyte;
+	int cache_size_byte;
+	int num_of_cache_block;
+	int set;
+	int num_of_set;
+
+	ifstream in("trace.txt");  // Open a file for text input.
+
+	if (!in) {
+		cout << "Cannot open file.\n";
+
+		return 1;
+	}
+
+	in >> hex >> a;
+	in >> hex >> b;
+	in >> hex >> c;
+
+	// Read trace.txt (Hint 1)
+	for (int i = 0; i < 5002; i++)
+	{
+		in >> hex >> d[i];
+	}
+
+	in.close();
+
+	cout << a << "\n";
+	cout << b << "\n";
+	cout << c << "\n";
+
+	// Show what the program read. (Hint 1)
+	for (int i = 0; i < 5002; i++)
+	{
+		cout << d[i] << "\n";
+	}
+
+	cout << "What is the block size of cache? (Byte)" << "\n";
+
+	cin >> block_size;
+
+	switch (block_size)
+	{
+	case 8:
+		for (int i = 0; i < 5002; i++)
+		{
+			location[i] = d[i] / 8;
+		}
+
+		break;
+
+	case 16:
+
+		for (int i = 0; i < 5002; i++)
+		{
+			location[i] = d[i] / 16;
+		}
+
+		break;
+
+	case 32:
+
+		for (int i = 0; i < 5002; i++)
+		{
+			location[i] = d[i] / 32;
+
+		}
+
+		break;
+
+	case 64:
+
+		for (int i = 0; i < 5002; i++)
+		{
+			location[i] = d[i] / 64;
+		}
+
+		break;
+
+	default:
+		break;
+	}
+
+	// Show the memory address' position in memory blocks. (Hint 2)
+	for (int i = 0; i < 5002; i++)
+	{
+		cout << location[i] << "\n";
+	}
+
+	cout << "What is the size of cache? (kilo-byte)" << "\n";
+	cin >> cache_size_kilobyte;
+
+	cache_size_byte = cache_size_kilobyte * 1024;
+	num_of_cache_block = cache_size_byte / block_size;
+
+
+	cout << "Specify n of n-set associative:" << "\n";
+	cin >> set;
+
+	num_of_set = num_of_cache_block / set;
+
+	return 0;
+}
