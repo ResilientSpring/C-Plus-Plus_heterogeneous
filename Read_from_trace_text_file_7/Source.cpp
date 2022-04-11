64ae8f96e09499b7be5d253997e7b91da25703ca
Ceiling_roof <ted10014@hotmail.com>
2022-04-11 13:45:00 +0800

    Committed 2022/04/11

---
+++ Read_from_trace_text_file_7/Source.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+	int a, b, c;
+
+	ifstream in("trace.txt");  // Open a file for text input.
+
+	if (!in) {
+		cout << "Cannot open file.\n";
+
+		return 1;
+	}
+
+	in >> hex >> a;
+	in >> hex >> b;
+	in >> hex >> c;
+
+	cout << dec << a << "\n";
+	cout << dec << b << "\n";
+	cout << dec << c << "\n";
+
+
+	in.close();
+
+	cout << a << "\n";
+	cout << b << "\n";
+	cout << c << "\n";
+
+	return 0;
+}
