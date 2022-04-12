71330afb512084dd2f8b7147f167df52774de245
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-04-12 12:16:16 +0800

    Committed 2022/04/12
    Using dynamic array

---
+++ Dynamic_array/Platitude.cpp
@@ -0,0 +1,34 @@
+// Allocate an array.
+
+#include <iostream>
+#include <new>
+using namespace std;
+
+int main() {
+	int* p, i;
+
+	try
+	{
+		p = new int[10]; // Allocate 10 integer array.
+	}
+	catch (bad_alloc xa)
+	{
+		cout << "Allocation Failure\n";
+		
+		return 1;
+	}
+
+	for ( i = 0; i < 10; i++)
+	{
+		p[i] = i;
+	}
+
+	for ( i = 0; i < 10; i++)
+	{
+		cout << p[i] << " ";
+	}
+
+	delete[] p; // release the array.
+
+	return 0;
+}
