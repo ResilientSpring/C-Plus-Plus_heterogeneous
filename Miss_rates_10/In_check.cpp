bfcb84e04e4f5e70dc08729f26e29a09707560e5
Ceiling_roof <48134466+ResilientSpring@users.noreply.github.com>
2022-04-30 17:10:55 +0800

    Committed 2022/04/30

--- Miss_rates_10/In_check.cpp
+++ Miss_rates_10/In_check.cpp
@@ -6,12 +6,19 @@
 using namespace std;
 
 // A cache block
-class block {
+class Block {
 	bool valid_bit = true;
 	int tag = -1;
+	int index = -1;
 	int count = 0;
 	int address = -1;
 	int address_in_decimal = -1;
+};
+
+class Set {
+	Set(int set_degree) {
+		Block block = 
+	}
 };
 
 int main() {
