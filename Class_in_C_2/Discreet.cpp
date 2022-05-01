cf937de66f7f8280bfb81ce4120e844681c433a7
Ceiling_roof <ted10014@hotmail.com>
2022-05-01 13:41:15 +0800

    Committed 2022/05/01

---
+++ Class_in_C_2/Discreet.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+	typedef struct student {
+		char name[20];
+		int ID;
+		int year;
+		char major[30];
+		float GPA;
+} Student;
+
+}
